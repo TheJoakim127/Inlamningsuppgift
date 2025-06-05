5b583d712a9eb43838dc4033f2b7a7a338fa8dfc
joaki <Joakim_holmstrom8@hotmail.com>
2025-06-05 10:19:57 +0200

    Uppdaterar uppgift

--- src/inlamningsuppgift/inlamningsuppgiftMain.java
+++ src/inlamningsuppgift/inlamningsuppgiftMain.java
@@ -18,10 +18,6 @@ public class inlamningsuppgiftMain {
 
         } while (loop);
 
-        System.out.println("Antal rader: " + nycounter.getTotalLines());
-        System.out.println("Antal tecken: " + nycounter.getTotalChar());
-        System.out.println("Antal ord: " + nycounter.getTotalWords());
-        System.out.println("Längsta ordet är: " + nycounter.getLongestWord());
-        System.out.println("Programmet är nu avslutat!");
+        System.out.println("Antal rader: " + nycounter.getTotalLines()+ "\n" + "Antal tecken: " + nycounter.getTotalChar() + "\n" + "Antal ord: " + nycounter.getTotalWords() + "\n" + "Längsta ordet är: " + nycounter.getLongestWord()+ "\n" + "Programmet är nu avslutat!");
     }
 }
