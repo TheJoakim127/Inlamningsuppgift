354836cfc0d3f65e24bb16e28f3253d0f9574288
joaki <Joakim_holmstrom8@hotmail.com>
2025-06-05 10:05:22 +0200

    Uppdaterar uppgift

--- src/inlamningsuppgift/inlamningsuppgiftMain.java
+++ src/inlamningsuppgift/inlamningsuppgiftMain.java
@@ -3,7 +3,7 @@ package inlamningsuppgift;
 
 import java.util.Scanner;
 
-public class inlamningsuppgift {
+public class inlamningsuppgiftMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Skriv din text. när du är klar skriv stop: ");
